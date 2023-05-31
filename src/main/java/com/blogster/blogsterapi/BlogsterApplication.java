fe09d8fa734766ac0876fd67341e93aa073db396
Krishna Vamsi <krishnavamc.chintalapati@gmail.com>
2023-05-31 22:13:46 +0530

    Initial Setup

---
+++ src/main/java/com/blogster/blogsterapi/BlogsterApplication.java
@@ -0,0 +1,13 @@
+package com.blogster.blogsterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogsterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogster/blogsterapi/BlogsterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogster.blogsterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogsterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
